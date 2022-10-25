ff18d5df825df2c6de648a77529f655ad400303d
LipeCorreia <felipenc413@gmail.com>
2022-10-25 20:20:40 -0300

    Adicionamento de metodo Delete e Get

--- src/main/java/system/demo/Controller.java
+++ src/main/java/system/demo/Controller.java
@@ -3,6 +3,7 @@ package system.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -24,6 +25,16 @@ public class Controller {
     public Optional<Cliente> getClienteById(@PathVariable Long id){
         Optional<Cliente> clienteReturned = repository.findById(id);
         return clienteReturned;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCLienteById(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
+    @GetMapping
+    public List<Cliente> listClientes(){
+        return repository.findAll();
     }
 
 
